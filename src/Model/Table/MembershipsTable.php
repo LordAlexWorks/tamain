63e994a345f5f9c4f1e687e0f4b4110a9ca490f2
Gabriela Scardine <scardinegabriela@gmail.com>
2016-05-31 18:31:17 -0300

    Create script to unsubscribe expired members, save exp_on in mailchimp

--- src/Model/Table/MembershipsTable.php
+++ src/Model/Table/MembershipsTable.php
@@ -106,34 +106,54 @@ class MembershipsTable extends Table
      */
     public function afterSave(Event $event, EntityInterface $entity, \ArrayObject $options)
     {
-        // Subscribe member to mailchimp list if no memberships are active
-        // or expired less than 15 days ago
         $expiration = new \DateTime($entity->expires_on);
 
+        // Active membership (not retroactive)
         if ($expiration >= new \DateTime) {
+            $member = $this->Members->findById($entity->member_id)->first();
+
+            $mailchimpKey = Configure::read('App.mailchimpKey');
+            $listId = "eaad0ec6e1";
+            $mc = new Mailchimp($mailchimpKey);
+
             $membershipsActiveOrRecent = $this->find('all')
                 ->where([
                     'Memberships.member_id =' => $entity->member_id,
                     'Memberships.expires_on >=' => date_format(new \DateTime("- 15 days"), 'Y-m-d')
                 ])
-                ->count();
+                ->order(['Memberships.expires_on' => 'DESC']);
 
-            // Only this membership is active
-            if ($membershipsActiveOrRecent <= 1) {
-                $mailchimpKey = Configure::read('App.mailchimpKey');
-                $listId = "eaad0ec6e1";
+            $highest_expiration_date = $membershipsActiveOrRecent->first()->expires_on;
 
-                $mc = new Mailchimp($mailchimpKey);
-                
-                $member = $this->Members->findById($entity->member_id)->first();
-                
+            // Subscribe member to mailchimp list if no memberships are active
+            // or expired less than 15 days ago
+            if ($membershipsActiveOrRecent->count() == 1) {
                 try {
                     $mc->post("lists/$listId/members", [
                         "status" => "subscribed",
                         "email_address" => $member->email,
                         "merge_fields" => [
                             "FNAME" => $member->firstname,
-                            "LNAME" => $member->lastname
+                            "LNAME" => $member->lastname,
+                            "EXP_ON" => date_format($expiration, 'Y-m-d')
+                        ]
+                    ]);
+                } catch (Exception $e) {
+                    if ($e->getMessage()) {
+                        $this->error = $e->getMessage();
+                    } else {
+                        $this->error = 'An unknown error occurred when registering user in Mailchimp.';
+                    }
+                }
+            }
+            // Already subscribed to mailchimp in previous membership
+            // Update expiration field if this one is higher
+            elseif ($entity->expires_on >= $highest_expiration_date) {
+                try {
+                    $subscriberHash = md5(strtolower($member->email));
+                    $mc->patch("lists/$listId/members/$subscriberHash", [
+                        "merge_fields" => [
+                            "EXP_ON" => date_format($expiration, 'Y-m-d')
                         ]
                     ]);
                 } catch (Exception $e) {
